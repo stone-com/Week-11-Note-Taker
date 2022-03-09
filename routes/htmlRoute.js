7e7581d158424e23cd2fe8ef080af1a7400c4250
Stone Comstock <comstockstone@yahoo.com>
2022-03-08 18:26:09 -0800

    Renamed 2 files

---
+++ routes/htmlRoute.js
@@ -0,0 +1,14 @@
+// require express router
+const router = require("express").Router();
+
+// Default route for GET Requests, redirect to index.html homepage
+router.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname, "../public.index.html"));
+});
+
+// Route for notes page GET request.
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+// exporting 
+module.exports = router;
