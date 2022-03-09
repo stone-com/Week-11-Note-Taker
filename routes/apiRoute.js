bedd165a7ea2720c18b8f90421a6a21f028c3f58
Stone Comstock <comstockstone@yahoo.com>
2022-03-08 22:55:34 -0800

    Added delete request to apiRoute

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,5 +1,6 @@
 // require express router and fs
 const router = require('express').router();
+const { parsedJSON } = require('express');
 const fs = require('fs');
 
 // GET Request
@@ -24,9 +25,44 @@ router.post('/notes', (req, res) => {
     // push the new note from the body of the post request to the parsedJSON
     parsedJSON.push(req.body);
     //  use writefile to write the array data to db.json
-    fs.writeFile("./db/db.json", JSON.stringify(parsedJSON), (err) => {
-        if(err) return err;
-        console.log("Success")
-    })
+    fs.writeFile('./db/db.json', JSON.stringify(parsedJSON), (err) => {
+      if (err) return err;
+      console.log('Successfully added');
+    });
   });
+  //   end the response
+  res.end();
 });
+
+// DELETE Request
+// User deletes a note and updates JSON data
+router.delete('/notes/:id', (req, res) => {
+  // get id from req.params.id and store as variable
+  let id = req.params.id;
+
+  // use readfile to get the json from the db file
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    // Throw error code of there was issue reading db.json
+    if (err) throw err;
+    // parse the json data and save it to a variable
+    let parsedJSON = JSON.parse(data);
+    // Loop through parsedJSON and look for matching ID's
+    for (let i = 0; i < parsedJSON.length; i++) {
+      //   if there is a match, use splice array method to remove it
+      if (id == parsedJSON[i].id) {
+        parsedJSON.splice(i, 1);
+        //  use writefile to overwrite the array data to db.json with the updated version
+        fs.writeFile('./db/db.json', JSON.stringify(parsedJSON), (err) => {
+          if (err) throw err;
+          // log "note deleted" to console/terminal
+          console.log('successfully deleted');
+        });
+      }
+    }
+  });
+
+  // end the response
+  res.end();
+});
+// export
+module.exports = router;
