8ebaaee5528101b21ef709288509300220be3c83
Stone Comstock <comstockstone@yahoo.com>
2022-03-09 17:35:08 -0800

    Bug fixes and cleaning code

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,11 +1,13 @@
 // require express router and fs
 const router = require('express').Router();
 const fs = require('fs');
+// require uuid to generate unique ids on each note
+const uuid = require('uuid');
 
 
 // GET Request
 // when user loads or reloads the page, it will parse the json from the db file so it can be displayed on the page
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
   // use readfile to get the json from the db file
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
@@ -14,55 +16,36 @@ router.get('/api/notes', (req, res) => {
   });
 });
 
+
 // POST Request
-// when user saves a note, it will ssave it to db.json file
+// When user wants to save a new note , it will parse the data and add a unique id onto the request body.
 router.post('/notes', (req, res) => {
-  // use readfile to get the json from the db file
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    // parse the json data and save it to a variable
-    let parsedJSON = JSON.parse(data);
-    // push the new note from the body of the post request to the parsedJSON
-    parsedJSON.push(req.body);
-    //  use writefile to write the array data to db.json
-    fs.writeFile('./db/db.json', JSON.stringify(parsedJSON), (err) => {
-      if (err) return err;
-      console.log('Successfully added');
-    });
-  });
-  //   end the response
-  res.end();
+  // read the json notes file and parse it.
+  const note = JSON.parse(fs.readFileSync('./db/db.json'));
+  // Save the request body to vairiable
+  const noteToAdd = req.body;
+  // generate  unique ID with uuid4, add it to request body
+  noteToAdd.id = uuid.v4();
+  // add new note to the json file
+  note.push(noteToAdd);
+  // write the file
+  fs.writeFileSync('./db/db.json', JSON.stringify(note));
+  // respond with new file
+  res.json(note);
 });
 
 // DELETE Request
-// User deletes a note and updates JSON data
-router.delete('/:id', (req, res) => {
-  // get id from req.params.id and store as variable
-  let id = req.params.id;
-
-  // use readfile to get the json from the db file
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    // Throw error code of there was issue reading db.json
-    if (err) throw err;
-    // parse the json data and save it to a variable
-    let parsedJSON = JSON.parse(data);
-    // Loop through parsedJSON and look for matching ID's
-    for (let i = 0; i < parsedJSON.length; i++) {
-      //   if there is a match, use splice array method to remove it
-      if (id == parsedJSON[i].id) {
-        parsedJSON.splice(i, 1);
-        //  use writefile to overwrite the array data to db.json with the updated version
-        fs.writeFile('./db/db.json', JSON.stringify(parsedJSON), (err) => {
-          if (err) throw err;
-          // log "note deleted" to console/terminal
-          console.log('successfully deleted');
-        });
-      }
-    }
-  });
-
-  // end the response
-  res.end();
+// when user clicks delete button
+router.delete('notes/:id', (req, res) => {
+  // read and parse the currrent db file
+  const note = JSON.parse(fs.readFileSync('./db/db.json'));
+  // filter through the notes for any where the id is not a match
+  const filteredNotes = note.filter((remove) => remove.id !== req.params.id);
+  // write the new notes 
+  fs.writeFileSync('./db/db.json', JSON.stringify(filteredNotes));
+  // respond with new json notes
+  res.json(filteredNotes);
 });
-// export
+
+
 module.exports = router;
